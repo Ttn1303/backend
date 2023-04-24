93093bcaffac4a932cad2462a2d9fda22d6136d2
phucvn <phucvn@newwave.com>
2023-04-24 09:36:45 +0700

    function search repair

--- app/Http/Controllers/RepairController.php
+++ app/Http/Controllers/RepairController.php
@@ -2,18 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Customer;
 use App\Models\Repair;
 use App\Models\RepairDetail;
-use App\Repositories\RepairRepository;
 use Illuminate\Http\Request;
 
 class RepairController extends Controller
 {
-
-    public function __construct(RepairRepository $repairRepo)
+    public function __construct(Repair $model)
     {
-        $this->repairRepo = $repairRepo;
+        $this->model = $model;
     }
     /**
      * Display a listing of the resource.
@@ -52,8 +49,6 @@ class RepairController extends Controller
                 return $query->select('*')->with(['Brand' => function ($sub) {
                     return $sub->select('id', 'name');
                 }]);
-                // ::with([
-                    
             },
             'Customer' => function ($query) {
                 return $query->select('id', 'name', 'phone', 'address');
@@ -62,7 +57,6 @@ class RepairController extends Controller
                 return $query->where('role', 'staff')->select('id', 'name');
             }
         ])->where('id', $id)->first();
-        // dd($repair);
         return response()->json([
             'result' => $repair
         ]);
@@ -78,7 +72,6 @@ class RepairController extends Controller
             ]);
         }
         RepairDetail::where('repair_id', $repair->id)->delete();
-        // dd($repair);
         $result = Repair::where('id', $repair->id)->delete();
         if ($result) {
             return response()->json([
@@ -96,13 +89,35 @@ class RepairController extends Controller
         return $request->all();
     }
 
-    public function search(Request $request) 
+    public function search(Request $request)
     {
-        // $options = $request->all();
-        // $this->
-        // return response()->json([
-        //     'result' => $repair
-        // ]);
+        $query = $this->model;
+        if ($request->state != '') {
+            $query = $query->where('state', $request->state);
+        }
+        if ($request->name_customer != '') {
+            $query = $query->with([
+                'Customer' => function ($query) use ($request) {
+                    return $query->select('*')->where('name', $request->name_customer);
+                }
+            ])
+                ->whereHas('Customer', function ($query) use ($request) {
+                    $query->where('customers.name', $request->name_customer);
+                });
+        }
+        if ($request->name_vehicle != '') {
+            $query = $query->with([
+                'VehicleInfor' => function ($query) use ($request) {
+                    return $query->select('*')->where('licensePlates', $request->name_vehicle);
+                }
+            ])
+                ->whereHas('VehicleInfor', function ($query) use ($request) {
+                    $query->where('vehicle_infors.licensePlates', $request->name_vehicle);
+                });
+        }
+         return response()->json([
+             'result' => $query->get()
+         ]);
     }
 
     /**
